eebc6c757eca72eec8691081620a75e537d59c3e
Yevhenii <ysych@masslight.com>
2023-10-03 17:12:57 +0300

    Added navigation controller dependency, fixed compatibility issues.

--- app/src/main/java/com/notesjc/ui/note/edit/NoteEditScreen.kt
+++ app/src/main/java/com/notesjc/ui/note/edit/NoteEditScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.notesjc.ui.note.list.NotesLazyList
--- app/src/main/java/com/notesjc/ui/note/list/NotesScreen.kt
+++ app/src/main/java/com/notesjc/ui/note/list/NotesScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.notesjc.model.Note
