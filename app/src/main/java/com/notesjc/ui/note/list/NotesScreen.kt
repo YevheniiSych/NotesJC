2666d892f88b6805d8045f8052424e8d8bd5db78
Yevhenii <ysych@masslight.com>
2023-10-09 18:35:08 +0300

    Added "Add note" button on notes screen.

--- app/src/main/java/com/notesjc/ui/note/list/NotesScreen.kt
+++ app/src/main/java/com/notesjc/ui/note/list/NotesScreen.kt
@@ -4,16 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.notesjc.model.Note
 import com.notesjc.ui.theme.Purple200
+import com.notesjc.ui.util.Screen
 
 @Composable
 fun NotesScreen(navController: NavController) {
-    Column(
+    Box(
         Modifier
             .fillMaxWidth()
             .fillMaxHeight()
@@ -24,11 +31,29 @@ fun NotesScreen(navController: NavController) {
             items = createItems(),
             navController = navController
         )
+
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            onClick = { navController.navigate(Screen.NoteEditScreen.route) },
+            backgroundColor = Color.Red,
+        ) {
+            Icon(Icons.Filled.Add, "Add note button button")
+        }
     }
 }
 
 fun createItems(): List<Note> {
     return listOf(
+        Note("1", "Hello World", "Creative dev"),
+        Note("2", "Hello World", "Creative dev"),
+        Note("1", "Hello World", "Creative dev"),
+        Note("2", "Hello World", "Creative dev"),
+        Note("1", "Hello World", "Creative dev"),
+        Note("2", "Hello World", "Creative dev"),
+        Note("1", "Hello World", "Creative dev"),
+        Note("2", "Hello World", "Creative dev"),
         Note("1", "Hello World", "Creative dev"),
         Note("2", "Hello World", "Creative dev"),
     )
